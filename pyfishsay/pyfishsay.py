f279901bc6503cbb77a34c26f47d7e6ffe0b8a3e
Hiroyuki Tanaka <ht0919@gmail.com>
2026-06-28 15:09:24 +0900

    Build Test 1

---
+++ pyfishsay/pyfishsay.py
@@ -0,0 +1,26 @@
+# pyfishsay : A joke app where fish talk
+# Ver       : 0.0.1
+# Auther    : Hiroyuki Tanaka
+
+import sys
+import random
+
+def main():
+    args = sys.argv
+    rand = ['','... < ギョギョギョ！ >',
+            '','... < ヒマだね～ >',
+            '']
+    msg1 = ''
+    msg2 = random.choice(rand)
+
+    for c in args[1]:
+        if   c == 'ご': msg1 += 'ぎょ'
+        elif c == 'ゴ': msg1 += 'ギョ'
+        else:           msg1 += c
+
+    print()
+    print('      ><(((°>', msg2 )
+    print(f'   ><(((°>   ><(((°> ... < { msg1 } >')
+
+if __name__ == "__main__":
+    main()
